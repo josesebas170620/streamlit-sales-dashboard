c7415d45333071f8e1ee56ff986c46fa09e2dadb
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-15 19:41:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def get_data_from_excel():
     )
     # Add 'hour' column to dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-    df["City"] = df["City"].fillna(0)
+    df["Total"] = df["Total"].fillna(0)
+    df["Rating"] = df["Rating"].fillna(0)
     return df
 
 df = get_data_from_excel()

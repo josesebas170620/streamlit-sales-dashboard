45f5103c87b032ee06f486e065d2db4599fb1569
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-15 19:30:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_data_from_excel():
     )
     # Add 'hour' column to dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-    df["city2"] = pd.to_numeric(df["City"], errors='coerce')
+    df["city2"] = pd.to_numeric(df["City"], errors='coerce').dt.city2
     return df
 
 df = get_data_from_excel()
